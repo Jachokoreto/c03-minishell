27b05c6ff8ea2a5963474785aa5b6b10f16bb51d
Lok Eu lee <leu-lee@u90z08s03.42kl.edu.my>
2022-03-20 12:10:09 +0800

    Add get_next_line and ./includes

--- includes/minishell.h
+++ includes/minishell.h
@@ -5,7 +5,11 @@
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include "get_next_line.h"
+# include "libft.h"
+# include <readline/readline.h>
 
-int	main(int argc, char **argv);
+int		main(int argc, char **argv);
+void	read_commands(char *line);
 
 #endif
