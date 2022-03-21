0bbb5dcc63fa237d4759b96ec1f47684bd87ff57
Jachokoreto <jatan@student.42kl.edu.my>
2022-03-21 18:24:59 +0800

    Add history to prompt

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leu-lee <leu-lee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 14:45:41 by leu-lee           #+#    #+#             */
-/*   Updated: 2022/03/21 14:47:43 by leu-lee          ###   ########.fr       */
+/*   Updated: 2022/03/21 18:23:32 by jatan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		line = readline("Minishell > ");
+		if (line && *line)
+			add_history(line);
 		read_commands(data, line);
 	}
 }
