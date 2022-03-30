5e89ce274f095999e10d7c252bf486419ed97069
Lok Eu lee <leu-lee@u91z08s04.42kl.edu.my>
2022-03-30 14:22:59 +0800

    Add main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: leu-lee <leu-lee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 14:45:41 by leu-lee           #+#    #+#             */
-/*   Updated: 2022/03/30 12:13:44 by leu-lee          ###   ########.fr       */
+/*   Updated: 2022/03/30 14:22:34 by leu-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		line = readline("minishell > ");
+		// heredoc("hello");
 		if (line == NULL)
 			exit(10);
 		if (line && *line)
